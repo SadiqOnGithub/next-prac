001cc218043382b31f3a202e95625d0a48a5a13e
sadiqonWSL <sadiqonemail@gmail.com>
2023-08-10 23:29:37 +0530

    testing post index route

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,18 @@
-export default function Posts() {
-  return <h1>is it working?</h1>
+import Layout from "../../components/layout";
+
+export default function Posts({lit}) {
+  console.log(lit);
+  return (
+    <Layout>
+      <h1>is it working?</h1>
+    </Layout>
+  )
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      lit : 2
+    },
+  }
 }
